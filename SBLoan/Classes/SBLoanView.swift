f369b3e3fe6fe815549083210413149dedee9c9c
Jonattan Sousa <jonattan.sousa@gmail.com>
2024-10-07 08:07:49 -0300

    First commit

---
+++ SBLoan/Classes/SBLoanView.swift
@@ -0,0 +1,21 @@
+//
+//  SBLoanView.swift
+//  Pods
+//
+//  Created by Jonattan Sousa on 04/10/24.
+//
+
+import SwiftUI
+
+public struct SBLoanView: View {
+    
+    public init() { }
+    
+    public var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SBLoanView()
+}
