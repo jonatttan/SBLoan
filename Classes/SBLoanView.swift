b8211aba72973e1738502bc71599d3dd43458c88
Jonattan Sousa <jonattan.sousa@gmail.com>
2024-10-22 08:28:06 -0300

    Apply view update

--- Classes/SBLoanView.swift
+++ Classes/SBLoanView.swift
@@ -73,7 +73,50 @@ public struct SBLoanView: View {
             .foregroundColor(.white)
             .clipShape(RoundedRectangle(cornerRadius: 7))
             .shadow(color: Color.gray.opacity(0.5), radius: 7, x: 0, y: 2)
-        
+            .overlay {
+                VStack(alignment: .leading) {
+                    headerCardView
+                        .padding(.top, 20)
+                        .padding(.horizontal)
+                    
+                    headerTextCardView
+                        .padding(.horizontal)
+                    
+                    Spacer()
+                }
+            }
+    }
+    
+    var headerCardView: some View {
+        VStack(alignment: .leading) {
+            Text("Veja sua opção de crédito pré aprovado")
+                .font(.system(size: 16))
+                .foregroundColor(.black)
+                .fontWeight(.medium)
+                .padding(.bottom, 5)
+            
+            HStack {
+                Image(systemName: "dollarsign.arrow.circlepath")
+                    .font(.system(size: 22))
+                
+                Text("Empréstimo pessoal")
+                    .font(.system(size: 16))
+                    .foregroundColor(.black)
+                    .fontWeight(.light)
+                
+                Spacer()
+            }
+            .padding(.bottom, 5)
+        }
+    }
+    
+    var headerTextCardView: some View {
+        HStack {
+            Text("Guardar dinheiro é o primeiro passo")
+                .font(.system(size: 12))
+                .foregroundColor(.black)
+                .fontWeight(.light)
+        }
     }
 }
 
